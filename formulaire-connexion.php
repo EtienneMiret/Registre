49a8223e1578c4ebdfa22e5f148e42c8af3de183
Étienne Miret <etienne.miret@ens-lyon.org>
2009-07-05 08:46:47 +0200

    Bouton personnalisé pour la connexion

--- formulaire-connexion.php
+++ formulaire-connexion.php
@@ -1,5 +1,5 @@
 <form action="Connexion" method="post">
 <p>Nom : <input type="text" name="id">
 <p>Mot de passe : <input type="password" name="pwd">
-<p><input type="submit">
+<p><button type="submit">Connexion</button>
 </form>
