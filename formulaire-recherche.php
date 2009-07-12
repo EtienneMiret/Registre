afc5573a55537d160a21278c61590280b0e1e9bc
Étienne Miret <etienne.miret@ens-lyon.org>
2009-07-12 11:19:25 +0200

    Le champ recherche de la page de résultats d’une recherche est prérempli avec celle-ci.

--- formulaire-recherche.php
+++ formulaire-recherche.php
@@ -1,5 +1,6 @@
 <form action="<?php echo $reg_racine; ?>Rechercher" method="get"
     class="recherche">
-<p><input name="q" type="text">
+<p><input name="q" type="text"<?php
+if(isset($q)) echo ' value="' . htmlspecialchars($q) . '"' ?>>
 <button type="submit">Rechercher</button>
 </form>
