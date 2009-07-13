5699af6568862272e2aadd6b3879fc57542f8b9f
Étienne Miret <etienne.miret@ens-lyon.org>
2009-07-13 15:58:31 +0200

    Petit texte pour expliquer l’utilité de la fonction recherche.

--- includes/formulaire-recherche.php
+++ includes/formulaire-recherche.php
@@ -1,6 +1,7 @@
 <form action="<?php echo $reg_racine; ?>Rechercher" method="get"
     class="recherche">
-<p><input name="q" type="text"<?php
+<p>Rechercher un film dans la base de donnée :<br>
+<input name="q" type="text"<?php
 if(isset($q)) echo ' value="' . htmlspecialchars($q) . '"' ?>>
 <button type="submit">Rechercher</button>
 </form>
