868431af6837f993c36bcdd50d0038e521df27a6
Étienne Miret <etienne.miret@ens-lyon.org>
2011-04-09 09:24:43 +0200

    config.php n'est plus suivi. Un fichier config_example est fournit comme guide pour créer config.php

--- config_example.php
+++ config_example.php
@@ -5,16 +5,16 @@
 /* Le serveur MySQL.
  * Il s’agit soit d’une addresse internet de la forme "hostname:port", soit du
  * chemin vers une socket locale, sous la forme : ":/path/to/socket". */
-$reg_serveur_bd = ':/private/var/mysql/mysql.sock';
+$reg_serveur_bd = 'localhost:3306';
 
 /* Nom de la base de données. */
-$reg_nom_bd = 'Registre_dev';
+$reg_nom_bd = 'registre';
 
 /* Nom d’utilisateur pour se connecter à la base de données. */
-$reg_utilisateur_bd = 'registre_dev';
+$reg_utilisateur_bd = 'registre';
 
 /* Mot de passe de l’utilisateur $reg_utilisateur_bd. */
-$reg_mdp_bd = 'dummypass';
+$reg_mdp_bd = '';
 
 /* Nom d'utilisateur pour administrer la base de données. Doit avoir les
  * droits CREATE et ALTER. */
@@ -26,7 +26,7 @@ $reg_mdp_admin_bd = $reg_mdp_bd;
 /* Addresse de la racine.
  * Il s’agit de du chemin vers la page d’accueil de Registre dans l’URL
  * (après le nom du serveur). */
-$reg_racine = '/Registre-DEV/';
+$reg_racine = '/Registre/';
 
 /* Nom de cette instance de Registre. */
-$reg_nom = 'Registre DÉVELOPPEMENT';
+$reg_nom = 'Registre';
