22de2d2a685f593b13a4f0725382fa298db57f9b
Étienne Miret <etienne.miret@ens-lyon.org>
2011-04-18 16:41:35 +0200

    On protège l'affichage de $reg_racine et $reg_nom. Ces deux variables peuvent très bien contenir des caractères spéciaux HTML.

--- includes/headers.php
+++ includes/headers.php
@@ -1,11 +1,13 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <?php if ($reg_titre_page=='Accueil' or $reg_titre_page=='') { ?>
-<title><?php echo $reg_nom; ?></title>
+<title><?php echo htmlspecialchars($reg_nom); ?></title>
 <?php } else { ?>
-<title><?php echo htmlspecialchars($reg_titre_page); ?> - <?php echo $reg_nom ?></title>
+<title><?php echo htmlspecialchars($reg_titre_page); ?> - <?php
+    echo htmlspecialchars($reg_nom) ?></title>
 <?php } ?>
-<link rel="stylesheet" type="text/css" href="<?php echo $reg_racine; ?>main">
+<link rel="stylesheet" type="text/css" href="<?php
+    echo htmlspecialchars($reg_racine); ?>main">
 <?php foreach($reg_head as $balise) {
     echo $balise . PHP_EOL;
 } ?>
