60fcf4bb9682d531eacb9b0220a3b3e6bf3e4a48
Rémi Couturier <remi.couturier@protonmail.com>
2011-05-09 20:32:06 +0200

    Intégration HTML / CSS de la page "Recherche avancée".

--- RechercheAvancee.php
+++ RechercheAvancee.php
@@ -16,33 +16,72 @@ $reg_onload='masquerChampsInutilises(document.getElementById("type").value);';
 
 require('includes/headers.php');
 ?>
-<form action="Rechercher" class="recherche-avancee">
-<p class="non-fini">Rechercher <select name="type" id="type"
-    onchange="masquerChampsInutilises(this.value);">
-  <option value="" selected>toutes les références</option>
-  <option value="Disque Blu-ray">les disques Blu-ray</option>
-  <option value="DVD">les DVD</option>
-  <option value="Cassette">les cassettes</option>
-  <option value="Livre">les livres</option>
-  <option value="BD">les BD</option>
-</select> qui vérifient les critères suivant :
-
-<ul>
-  <li><label>N’importe quel champ contient : <input name="q"></label>
-  <li><label>Titre : <input name="titre"></label>
-  <li class="film"><label>Réalisateur : <input name="realisateur"></label>
-  <li class="film"><label>Acteurs : <input name="acteur"></label>
-  <li class="film"><label>Compositeur : <input name="compositeur"></label>
-  <li class="livre"><label>Auteur : <input name="auteur"></label>
-  <li class="bd"><label>Dessinateur : <input name="dessinateur"></label>
-  <li class="bd"><label>Scénariste : <input name="scenariste"></label>
-  <li class="bd"><label>Série : <input name="serie"></label>
-  <li><label>Commentaire : <input name="commentaire"></label>
-  <li><label>Proprietaire : <input name="proprietaire"></label>
-  <li><label>Emplacement : <input name="emplacement"></label>
-  <li><label>Enregistré par : <input name="createur"></label>
-  <li><label>Dernière modification par : <input name="editeur"></label>
-</ul>
-<p><button>Rechercher</button>
-</form>
+	<form action="Rechercher" class="recherche-avancee">
+		<p>Rechercher 
+			<select name="type" id="type" onchange="masquerChampsInutilises(this.value);">
+				<option value="" selected>toutes les références</option>
+				<option value="Disque Blu-ray">les disques Blu-ray</option>
+				<option value="DVD">les DVD</option>
+				<option value="Cassette">les cassettes</option>
+				<option value="Livre">les livres</option>
+				<option value="BD">les BD</option>
+			</select> qui vérifient les critères suivant :
+		</p>
+		<ul>
+			<li class="form-row">
+				<label for="q">N’importe quel champ contient :</label>
+				<input type="text" name="q" id="q"/>
+			</li>
+			<li class="film form-row">
+				<label for="realisateur">Réalisateur :</label>
+				<input type="text" name="realisateur" id="realisateur"/>
+			</li>
+			<li class="film form-row">
+				<label for="acteur">Acteurs :</label>
+				<input type="text" name="acteur" id="acteur"/>
+			</li>
+			<li class="film form-row">
+				<label for="compositeur">Compositeur :</label>
+				<input type="text" name="compositeur" id="compositeur"/>
+			</li>
+			<li class="livre form-row">
+				<label for="auteur">Auteur :</label>
+				<input type="text" name="auteur" id="auteur"/>
+			</li>
+			<li class="bd form-row">
+				<label for="dessinateur">Dessinateur :</label>
+				<input type="text" name="dessinateur" id="dessinateur"/>
+			</li>
+			<li class="bd form-row">
+				<label for="scenariste">Scénariste :</label>
+				<input type="text" name="scenariste" id="scenariste"/>
+			</li>
+			<li class="bd form-row">
+				<label for="serie">Série :</label>
+				<input type="text" name="serie" id="serie"/>
+			</li>
+			<li class="form-row">
+				<label for="commentaire">Commentaire :</label>
+				<input type="text" name="commentaire" id="commentaire"/>
+			</li>
+			<li class="form-row">
+				<label for="proprietaire">Proprietaire :</label>
+				<input type="text" name="proprietaire" id="proprietaire"/>
+			</li>
+			<li class="form-row">
+				<label for="emplacement">Emplacement :</label> 
+				<input type="text" name="emplacement" id="emplacement"/>
+			</li>
+			<li class="form-row">
+				<label for="createur">Enregistré par :</label>
+				<input type="text" name="createur" id="createur"/>
+			</li>
+			<li class="form-row">
+				<label for="editeur">Dernière modification par :</label>
+				<input type="text" name="editeur" id="editeur"/>
+			</li>
+		</ul>
+		<button>Rechercher</button>
+	</form>
+	
 <?php require('includes/footer.php'); ?>
