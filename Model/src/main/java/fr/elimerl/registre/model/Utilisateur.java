6df6de9cb5a9a3b955530d8d266097f0d5223893
Etienne Miret <etienne.miret@ens-lyon.org>
2012-11-29 07:28:03 +0100

    Modèle : Création de la classe Utilisateur.

---
+++ Model/src/main/java/fr/elimerl/registre/model/Utilisateur.java
@@ -0,0 +1,185 @@
+package fr.elimerl.registre.model;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Un utilisateur de l’application.
+ */
+@Entity
+@Table(name = "utilisateurs")
+public class Utilisateur {
+
+    /**
+     * Taille du sel (nombre de caractères de la représentation hexadécimale).
+     */
+    private static final int TAILLE_SEL = 8;
+
+    /**
+     * Encodage de caractère utilisé 
+     */
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
+    /**
+     * Générateur de nombres aléatoires utilisé pour générer les sels.
+     */
+    private static final Random random = new Random();
+
+    /**
+     * Identifiant de cet objet dans la base de données.
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    /**
+     * Nom de cet utilisateur.
+     */
+    @Column(name = "nom")
+    private String nom;
+
+    /**
+     * Sel utilisé pour stocker le mot de passe. Doit faire {@value #TAILLE_SEL}
+     * caractères de long.
+     */
+    @Column(name = "sel")
+    private String sel;
+
+    /**
+     * Hash MD5 du mot de passe salé. Est constitué du {@link #sel}, du
+     * caractère ‘:’ et du vrai mot de passe, concaténés et hachés. 
+     */
+    @Column(name = "mdp")
+    private String hashMdp;
+
+    /**
+     * Constructure sans arguments, utilisé par Hibernate.
+     */
+    public Utilisateur() {
+	super();
+    }
+
+    /**
+     * Construit un nouvel utilisateur, avec le nom et le mot de passe donnés en
+     * paramètres.
+     *
+     * @param nom
+     *            nom de cet utilisateur.
+     * @param mdp
+     *            mot de passe initial de cet utilisateur.
+     */
+    public Utilisateur(final String nom, final String mdp) {
+	super();
+	this.nom = nom;
+	définirMdp(mdp);
+    }
+
+    /**
+     * Change le mot de passe de cet utilisateur.
+     *
+     * @param mdp
+     *            nouveau mot de passe de cet utilisateur.
+     */
+    public void définirMdp(final String mdp) {
+	sel = selAléatoire();
+	hashMdp = hacherMdp(mdp);
+    }
+
+    /**
+     * Vérifie que le mot de passe donné en argument est correct. Le mot de
+     * passe est d’abord normalisé, ce qui permet de prendre en charge les
+     * accents de manière fiable.
+     *
+     * @param mdp
+     *            mot de passe à vérifier.
+     * @return {@code true} si {@code mdp} est bien le mot de passe de cet
+     *         utilisateur, {@code false} sinon.
+     */
+    public boolean vérifierMdp(final String mdp) {
+	return hashMdp.equals(hacherMdp(mdp));
+    }
+
+    /**
+     * Renvoie le nom de cet utilisateur.
+     *
+     * @return le nom de cet utilisateur.
+     */
+    public String getNom() {
+	return nom;
+    }
+
+    @Override
+    public String toString() {
+	return "Utilisateur:" + nom;
+    }
+
+    /**
+     * Génère un sel aléatoire, sous forme de {@value #TAILLE_SEL} caractères
+     * hexadécimaux.
+     *
+     * @return un sel aléatoire.
+     * @see #sel
+     */
+    private String selAléatoire() {
+	final int sel;
+	synchronized (random) {
+	    sel = random.nextInt();
+	}
+	final String baseSel = Integer.toHexString(sel);
+	final StringBuilder selBuilder = new StringBuilder(TAILLE_SEL);
+	for (int i = baseSel.length(); i < TAILLE_SEL; i++) {
+	    selBuilder.append('0');
+	}
+	selBuilder.append(baseSel);
+	return selBuilder.toString();
+    }
+
+    /**
+     * Hache un mot de passe avec le sel de cet utilisateur. Renvoie une
+     * représentation hexadécimale du hash calculé.
+     *
+     * @param mdp
+     *            mot de passe à hacher.
+     * @return le hash en hexadécimal du mot de passe donné.
+     * @see #hashMdp
+     */
+    private String hacherMdp(final String mdp) {
+	/* Saler le mot de passe. */
+	final String nfkcMdp = Normalizer.normalize(mdp, Form.NFKC);
+	final StringBuilder saléBuilder =
+		new StringBuilder(sel.length() + 1 + nfkcMdp.length());
+	saléBuilder.append(sel);
+	saléBuilder.append(':');
+	saléBuilder.append(nfkcMdp);
+	final String salé = saléBuilder.toString();
+
+	/* Hacher le mot de passe. */
+	final MessageDigest md5;
+	try {
+	    md5 = MessageDigest.getInstance("MD5");
+	} catch (NoSuchAlgorithmException e) {
+	    throw new RuntimeException("Pas de MD5.", e);
+	}
+	md5.update(salé.getBytes(UTF_8));
+	final byte[] hash = md5.digest();
+
+	/* Convertir le hash en héxadécimal. */
+	final StringBuilder hexBuilder = new StringBuilder(hash.length * 2);
+	for (final byte b : hash) {
+	    hexBuilder.append(String.format("%2x", Byte.valueOf(b)));
+	}
+	return hexBuilder.toString();
+    }
+
+}
---
+++ Model/src/test/java/fr/elimerl/registre/model/UtilisateurTest.java
@@ -0,0 +1,97 @@
+package fr.elimerl.registre.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test de la classe {@link Utilisateur}.
+ */
+public class UtilisateurTest {
+
+    /** Nom de l’utilisateur dans ces tests. */
+    private static final String NOM = "Etienne";
+
+    /** Mot de passe initial de l’utilisateur pour ces tests. */
+    private static final String MOT_DE_PASSE_INITIAL = "qwerty";
+
+    /**
+     * Exemples de mots de passe. Tous différents de
+     * {@link #MOT_DE_PASSE_INITIAL} et différents les uns des autres.
+     */
+    private static final String[] MOTS_DE_PASSE =
+	    new String[] { "azerty", "AZERTY", "Eoqk", "9Juj","a2(;Xz" };
+
+    /**
+     * Un seul mot de passe encodé de manières différentes.
+     */
+    private static final String[] MOT_DE_PASSE_ENCODÉ =
+	    new String[] {
+		"stéèàñÑçff",
+		"ﬆéèàñÑçﬀ",
+		// st     é     è     à     ñ      Ñ    ff
+		"\ufb06\u00e9\u00e8\u00e0\u00f1\u00d1ç\ufb00",
+		// st  e   ´  e   `  a   `  n   ~  N   ~  c   ¸    ff
+		"\ufb06e\u0301e\u0300a\u0300n\u0303N\u0303c\u0327\ufb00" };
+
+    /**
+     * Teste la création d’un utilisateur, ainsi que les méthodes
+     * {@link Utilisateur#getNom() getNom()} et
+     * {@link Utilisateur#vérifierMdp(String) vérifierMdp()}.
+     */
+    @Test
+    public void testCréation() {
+	final Utilisateur utilisateur =
+		new Utilisateur(NOM, MOT_DE_PASSE_INITIAL);
+	assertEquals("Il y a un problème dans le nom de l’utilisateur.",
+		NOM, utilisateur.getNom());
+	assertTrue("Problème de vérification du mot de passe.",
+		utilisateur.vérifierMdp(MOT_DE_PASSE_INITIAL));
+	for (final String exempleMdp : MOTS_DE_PASSE) {
+	    assertFalse("Un faux mot de passe est accepté.",
+		    utilisateur.vérifierMdp(exempleMdp));
+	}
+    }
+
+    /**
+     * Teste le changement de mot de passe avec la méthode
+     * {@link Utilisateur#définirMdp(String) définirMdp(String)}.
+     */
+    @Test
+    public void testDéfinirMotDePasse() {
+	final Utilisateur utilisateur =
+		new Utilisateur(NOM, MOT_DE_PASSE_INITIAL);
+	for (final String mdp : MOTS_DE_PASSE) {
+	    utilisateur.définirMdp(mdp);
+	    for (final String mdpTest : MOTS_DE_PASSE) {
+		if (mdp.equals(mdpTest)) {
+		    assertTrue("Le bon mot de passe n’est pas reconnu.",
+			    utilisateur.vérifierMdp(mdpTest));
+		} else {
+		    assertFalse("Un faux mot de passe est accepté.",
+			    utilisateur.vérifierMdp(mdpTest));
+		}
+	    }
+	}
+    }
+
+    /**
+     * Teste la prise en charge des mots de passe contenant des caractères
+     * non-ascii. On veut nottament qu’un même mot de passe encodé de manières
+     * différentes soit reconnu.
+     */
+    @Test
+    public void testMotDePasseEncodé() {
+	final Utilisateur utilisateur =
+		new Utilisateur(NOM, MOT_DE_PASSE_INITIAL);
+	for (int i = 0; i < MOT_DE_PASSE_ENCODÉ.length; i++) {
+	    utilisateur.définirMdp(MOT_DE_PASSE_ENCODÉ[i]);
+	    for (int j = 0; j < MOT_DE_PASSE_ENCODÉ.length; j++) {
+		assertTrue("Problème de vérification des mots de passe non-ascii : "
+			+ i + "≠" + j,
+			utilisateur.vérifierMdp(MOT_DE_PASSE_ENCODÉ[j]));
+	    }
+	}
+    }
+
+}
