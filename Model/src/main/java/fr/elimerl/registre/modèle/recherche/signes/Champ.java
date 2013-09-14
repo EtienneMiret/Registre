3d7b68a1d28399f9ab9df0a51278220258713b43
Etienne Miret <etienne.miret@ens-lyon.org>
2013-09-14 06:00:25 +0200

    Ajout de quelques champs parmis les signes.

--- Model/src/main/java/fr/elimerl/registre/modèle/recherche/signes/Champ.java
+++ Model/src/main/java/fr/elimerl/registre/modèle/recherche/signes/Champ.java
@@ -1,8 +1,8 @@
 package fr.elimerl.registre.modèle.recherche.signes;
 
 /**
- * Ce signe représente une catégorie dans laquelle chercher un mot clé. Par
- * exemple le titre, le commentaire ou l’auteur.
+ * Ce signe représente un champ dans lequel chercher un mot clé. Par exemple le
+ * titre, le commentaire ou l’auteur.
  */
 public final class Champ extends Signe {
 
@@ -11,6 +11,33 @@ public final class Champ extends Signe {
 
     /** Le commentaire associé à une fiche. */
     public static final Champ COMMENTAIRE = new Champ("commentaire:");
+
+    /** La série dont fait partie une fiche. */
+    public static final Champ SÉRIE = new Champ("série:");
+
+    /** Le propriétaire d’une fiche. */
+    public static final Champ PROPRIÉTAIRE = new Champ("propriétaire:");
+
+    /** L’emplacement où est rangé une fiche. */
+    public static final Champ EMPLACEMENT = new Champ("emplacement:");
+
+    /** Le réalisateur d’un film. */
+    public static final Champ RÉALISATEUR = new Champ("réalisateur:");
+
+    /** Un acteur qui joue dans un film. */
+    public static final Champ ACTEUR = new Champ("acteur:");
+
+    /** Le compositeur de la musique d’un film. */
+    public static final Champ COMPOSITEUR = new Champ("compositeur:");
+
+    /** Le dessinateur d’une bande-dessinée. */
+    public static final Champ DESSINATEUR = new Champ("dessinateur:");
+
+    /** Le scénariste d’une bande-dessinée. */
+    public static final Champ SCÉNARISTE = new Champ("scénarise:");
+
+    /** L’auteur d’un livre. */
+    public static final Champ AUTEUR = new Champ("auteur:");
 
     /**
      * Constructeur privé, seul un nombre prédéfini d’instance étant autorisées.
