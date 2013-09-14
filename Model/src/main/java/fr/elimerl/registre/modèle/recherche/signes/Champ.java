3ff32d3ea23ada925e79bc82e9068876b360466c
Etienne Miret <etienne.miret@ens-lyon.org>
2013-09-14 05:48:16 +0200

    « Catégorie » renommée en « Champ ».

--- Model/src/main/java/fr/elimerl/registre/modèle/recherche/signes/Champ.java
+++ Model/src/main/java/fr/elimerl/registre/modèle/recherche/signes/Champ.java
@@ -4,13 +4,13 @@ package fr.elimerl.registre.modèle.recherche.signes;
  * Ce signe représente une catégorie dans laquelle chercher un mot clé. Par
  * exemple le titre, le commentaire ou l’auteur.
  */
-public final class Catégorie extends Signe {
+public final class Champ extends Signe {
 
     /** Le titre d’une fiche. */
-    public static final Catégorie TITRE = new Catégorie("titre:");
+    public static final Champ TITRE = new Champ("titre:");
 
     /** Le commentaire associé à une fiche. */
-    public static final Catégorie COMMENTAIRE = new Catégorie("commentaire:");
+    public static final Champ COMMENTAIRE = new Champ("commentaire:");
 
     /**
      * Constructeur privé, seul un nombre prédéfini d’instance étant autorisées.
@@ -18,7 +18,7 @@ public final class Catégorie extends Signe {
      * @param représentation
      *            chaîne de caractères représentant la catégorie.
      */
-    private Catégorie(final String représentation) {
+    private Champ(final String représentation) {
 	super(représentation);
     }
 
