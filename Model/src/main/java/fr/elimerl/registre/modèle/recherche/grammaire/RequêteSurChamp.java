3da333bf5c7bb2c0eefcb211589c4b6bd1edf978
Etienne Miret <etienne.miret@ens-lyon.org>
2013-09-20 20:24:45 +0200

    Style (suppression d’une espace).

--- Model/src/main/java/fr/elimerl/registre/modèle/recherche/grammaire/RequêteSurChamp.java
+++ Model/src/main/java/fr/elimerl/registre/modèle/recherche/grammaire/RequêteSurChamp.java
@@ -122,7 +122,7 @@ public final class RequêteSurChamp<T> extends Expression {
 		    requête.subquery(champ.getClasse());
 	    final Root<T> type = sousRequête.from(champ.getClasse());
 	    final Predicate comme = constructeur.like(
-		    type.<String> get("nom"), mot);
+		    type.<String>get("nom"), mot);
 	    sousRequête.where(comme);
 	    prédicats[i] = constructeur.in(fiche.get(champ.getNom())).value(
 		    sousRequête);
