afb7372c7db3b8b7cb49941d845abc8a24de510c
Etienne Miret <etienne.miret@ens-lyon.org>
2014-02-26 21:58:45 +0100

    Création de la classe principale du programe de reprise de données.

---
+++ RepriseDeDonnées/src/main/java/fr/elimerl/registre/reprise/RepriseDeDonnées.java
@@ -0,0 +1,12 @@
+package fr.elimerl.registre.reprise;
+
+/**
+ * Classe principale du programme de reprise de données.
+ */
+public class RepriseDeDonnées {
+
+    public static void main(String[] args) {
+	System.out.println("Pas encore implémenté.");
+    }
+
+}
