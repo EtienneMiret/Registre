f568c032e46ce94554355cc636713fcca3d5c77c
Etienne Miret <etienne.miret@ens-lyon.org>
2014-08-10 18:24:25 +0200

    Mise en place de la page de recherche.

---
+++ Registre/src/main/java/fr/elimerl/registre/controleurs/Recherche.java
@@ -0,0 +1,65 @@
+package fr.elimerl.registre.controleurs;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.elimerl.registre.entités.Fiche;
+import fr.elimerl.registre.recherche.grammaire.Requête;
+import fr.elimerl.registre.services.ParseurDeRecherches;
+
+/**
+ * Contrôleur chargé de traiter les recherches faites par un utilisateur.
+ */
+@Controller
+public class Recherche {
+
+    /**
+     * Gestionnaire d’entité JPA, injecté par Spring.
+     */
+    @PersistenceContext(name = "Registre")
+    private EntityManager em;
+
+    /**
+     * Service parseur de recherches, injecté par Spring.
+     */
+    @Autowired
+    private ParseurDeRecherches parseur;
+
+    private CriteriaBuilder constructeur;
+
+    @PostConstruct
+    public void setUp() {
+	constructeur = em.getCriteriaBuilder();
+    }
+
+    @RequestMapping("/Rechercher/{texte}")
+    @Transactional(readOnly = true)
+    public String rechercher(@PathVariable final String texte,
+	    final Model modèle) {
+	final Requête requêteRegistre = parseur.analyser(texte);
+	final CriteriaQuery<Fiche> requête =
+		constructeur.createQuery(Fiche.class);
+	final Root<Fiche> fiche = requête.from(Fiche.class);
+	final Predicate prédicat =
+		requêteRegistre.créerPrédicat(constructeur, requête, fiche);
+	requête.select(fiche);
+	requête.where(prédicat);
+	final TypedQuery<Fiche> requêteJpa = em.createQuery(requête);
+	modèle.addAttribute("fiches", requêteJpa.getResultList());
+	return "recherche";
+    }
+
+}
