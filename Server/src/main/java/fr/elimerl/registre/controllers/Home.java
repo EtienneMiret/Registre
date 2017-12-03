98bf9fd1a14f1c56573aaa19b52715453321e626
Etienne Miret <etienne.miret@ens-lyon.org>
2017-12-03 16:48:13 +0100

    22 Get the authentication token from Thymeleaf Spring Security integration module.
    Note that thymeleaf-extras-springsecurity5 doesn’t exist at the
    moment. So we’re using thymeleaf-extras-springsecurity4 with Spring
    Security 5, and it works like a charm.

--- Server/src/main/java/fr/elimerl/registre/controllers/Home.java
+++ Server/src/main/java/fr/elimerl/registre/controllers/Home.java
@@ -1,13 +1,8 @@
 package fr.elimerl.registre.controllers;
 
-import fr.elimerl.registre.security.RAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Controller for the home page.
@@ -16,11 +11,8 @@ import java.util.Map;
 public class Home {
 
   @RequestMapping("/")
-  public ModelAndView home (Principal principal) {
-    Map<String, Object> model = new HashMap<> ();
-    RAuthenticationToken token = (RAuthenticationToken) principal;
-    model.put ("user", token.getPrincipal ());
-    return new ModelAndView ("home", model);
+  public ModelAndView home () {
+    return new ModelAndView ("home");
   }
 
 }
