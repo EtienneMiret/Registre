d3eb26bc947aca44ea1b04caee5bb6d48d13e13d
Etienne Miret <etienne.miret@ens-lyon.org>
2019-03-30 22:07:47 +0100

    Migrate to TypeScript (#30).

--- Server/src/main/ts/editor.ts
+++ Server/src/main/ts/editor.ts
@@ -1,30 +1,32 @@
-function hideUnusedFields (form, type) {
+function hideUnusedFields (form: HTMLFormElement, type: string) {
   hideExcept (form, type, 'comic');
   hideExcept (form, type, 'movie');
   hideExcept (form, type, 'book');
 }
 
-function hideExcept (form, typeToKeep, type) {
-  const elements = form.getElementsByClassName (type);
+function hideExcept (form: HTMLFormElement, typeToKeep: string, type: string) {
+  const elements =
+      <HTMLCollectionOf<HTMLElement>>form.getElementsByClassName (type);
   for (let i = 0; i < elements.length; i++) {
     elements.item (i).hidden = typeToKeep !== type;
   }
 }
 
-function addActor (actorsList) {
+function addActor (actorsList: HTMLOListElement|HTMLUListElement) {
   const index = actorsList
       .getElementsByTagNameNS ('http://www.w3.org/1999/xhtml', 'li')
       .length;
   const li = document.createElementNS ('http://www.w3.org/1999/xhtml', 'li');
-  const input = document.createElementNS ('http://www.w3.org/1999/xhtml', 'input');
+  const input =
+      <HTMLInputElement>document.createElementNS ('http://www.w3.org/1999/xhtml', 'input');
   input.name = `actors[${index}]`;
   li.appendChild (input);
   actorsList.appendChild (li);
 }
 
 function ready () {
-  const form = document.getElementById ('form');
-  hideUnusedFields (form, form.elements.type.value);
+  const form = <HTMLFormElement>document.getElementById ('form');
+  hideUnusedFields (form, form.elements['type'].value);
   document.getElementById ('add-actor').hidden = false;
 }
 
