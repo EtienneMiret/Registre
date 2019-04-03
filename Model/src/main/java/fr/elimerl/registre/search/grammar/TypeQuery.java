a06adbd1a8ffcf74cfbf41de5d04434eefc09ba2
Etienne Miret <etienne.miret@ens-lyon.org>
2019-04-04 00:22:10 +0200

    Create TypeQuery expressions (#27).

---
+++ Model/src/main/java/fr/elimerl/registre/search/grammar/TypeQuery.java
@@ -0,0 +1,52 @@
+package fr.elimerl.registre.search.grammar;
+
+import fr.elimerl.registre.entities.Record;
+import fr.elimerl.registre.search.tokens.Type;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Objects;
+
+/**
+ * Kind of {@link Expression} that models a query targeting a specific subtype
+ * of {@link Record}.
+ */
+public final class TypeQuery extends Expression {
+
+  private final Type type;
+
+  public TypeQuery (Type type) {
+    this.type = type;
+  }
+
+  @Override
+  public Predicate createPredicate (
+      CriteriaBuilder builder,
+      CriteriaQuery<Record> query,
+      Root<Record> root
+  ) {
+    return null;
+  }
+
+  @Override
+  public boolean equals (Object other) {
+    if (other == this) {
+      return true;
+    } else if (other == null) {
+      return false;
+    } else if (other instanceof TypeQuery) {
+      TypeQuery otherQuery = (TypeQuery) other;
+      return this.type == otherQuery.type;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public int hashCode () {
+    return Objects.hashCode (type);
+  }
+
+}
