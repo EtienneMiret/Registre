7bd4dee1b6a3c95bcb0256367e0db9c677bbc59c
Etienne Miret <etienne.miret@ens-lyon.org>
2019-04-03 23:11:31 +0200

    Reformat Operator.java (#27).

--- Model/src/main/java/fr/elimerl/registre/search/tokens/Operator.java
+++ Model/src/main/java/fr/elimerl/registre/search/tokens/Operator.java
@@ -9,28 +9,28 @@ import java.util.List;
  */
 public final class Operator extends Token {
 
-    /** The boolean “OR”. */
-    public static final Operator OR = new Operator("or");
+  /** The boolean “OR”. */
+  public static final Operator OR = new Operator ("or");
 
-    /**
-     * Returns all existing operators.
-     *
-     * @return all existing operators.
-     */
-    public static List<Operator> all() {
-	final List<Operator> result = new ArrayList<Operator>(1);
-	result.add(OR);
-	return result;
-    }
+  /**
+   * Returns all existing operators.
+   *
+   * @return all existing operators.
+   */
+  public static List<Operator> all () {
+    final List<Operator> result = new ArrayList<Operator> (1);
+    result.add (OR);
+    return result;
+  }
 
-    /**
-     * Private constructor, since only a limited number of instances is allowed.
-     *
-     * @param representation
-     *          keyword of the query language matching this operator.
-     */
-    private Operator(final String representation) {
-	super(representation);
-    }
+  /**
+   * Private constructor, since only a limited number of instances is allowed.
+   *
+   * @param representation
+   *          keyword of the query language matching this operator.
+   */
+  private Operator (final String representation) {
+    super (representation);
+  }
 
 }
