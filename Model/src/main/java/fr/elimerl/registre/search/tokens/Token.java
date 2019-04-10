2938b0a8abc7f3f7978b18c3100bc12eab25fb6b
Etienne Miret <etienne.miret@ens-lyon.org>
2019-04-10 22:51:23 +0200

    Reindent Token.java (#60).

--- Model/src/main/java/fr/elimerl/registre/search/tokens/Token.java
+++ Model/src/main/java/fr/elimerl/registre/search/tokens/Token.java
@@ -7,38 +7,40 @@ import java.util.regex.Pattern;
  */
 public abstract class Token {
 
-    /** This token as a string. */
-    private final String value;
+  /** This token as a string. */
+  private final String value;
 
-    /**
-     * The pattern that allows detection of this token in a string.
-     */
-    private final Pattern pattern;
+  /**
+   * The pattern that allows detection of this token in a string.
+   */
+  private final Pattern pattern;
 
-    /**
-     * Create a new sign from its string value.
-     *
-     * @param value
-     *          this new pattern’s value as a string.
-     */
-    public Token(final String value) {
-	this.value = value;
-	this.pattern = Pattern.compile("\\G\\s*" + Pattern.quote(value),
-		Pattern.CASE_INSENSITIVE);
-    }
+  /**
+   * Create a new sign from its string value.
+   *
+   * @param value
+   *     this new pattern’s value as a string.
+   */
+  public Token (final String value) {
+    this.value = value;
+    this.pattern = Pattern.compile (
+        "\\G\\s*" + Pattern.quote (value),
+        Pattern.CASE_INSENSITIVE
+    );
+  }
 
-    /**
-     * Returns the pattern that allows detection of this token in a string.
-     *
-     * @return the pattern that allows detection of this token in a string.
-     */
-    public Pattern getPattern() {
-	return pattern;
-    }
+  /**
+   * Returns the pattern that allows detection of this token in a string.
+   *
+   * @return the pattern that allows detection of this token in a string.
+   */
+  public Pattern getPattern () {
+    return pattern;
+  }
 
-    @Override
-    public String toString() {
-	return value;
-    }
+  @Override
+  public String toString () {
+    return value;
+  }
 
 }
