4bc10d3607a32f1dcab50d1be542e44d0b6c5819
Étienne Miret <etienne.miret@ens-lyon.org>
2021-10-23 20:22:25 +0200

    Setup Google authentication.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.scss'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({Component, pageProps}: AppProps) {
+  return <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+  </SessionProvider>;
 }
 export default MyApp
