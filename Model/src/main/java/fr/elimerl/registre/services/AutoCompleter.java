69a649c6e1e1c64f3fce5e5301bac09dd3211505
Étienne Miret <etienne.miret@ens-lyon.org>
2022-05-01 14:56:19 +0200

    List referenced actors and directors (#72).

--- Model/src/main/java/fr/elimerl/registre/services/AutoCompleter.java
+++ Model/src/main/java/fr/elimerl/registre/services/AutoCompleter.java
@@ -1,13 +1,11 @@
 package fr.elimerl.registre.services;
 
-import fr.elimerl.registre.entities.Named;
+import fr.elimerl.registre.entities.*;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
+import javax.persistence.criteria.*;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -72,6 +70,59 @@ public class AutoCompleter {
         .filter (res -> regex.matcher (res).find ())
         .limit (MAX_SUGGESTIONS)
         .collect (toList ());
+  }
+
+  /**
+   * List all referenced actors.
+   *
+   * @return all referenced actors.
+   * @see #list(Class, Class, String)
+   */
+  public List<Actor> listActors() {
+    return list(Actor.class, Movie.class, "actors");
+  }
+
+  /**
+   * List all referenced directors.
+   *
+   * @return all referenced directors.
+   * @see #list(Class, Class, String)
+   */
+  public List<Director> listDirectors() {
+    return list(Director.class, Movie.class, "director");
+  }
+
+  /**
+   * List all {@link Named} of a given type. Items with no reference are omitted.
+   *
+   * @param resultClass
+   *     class of {@link Named} to list.
+   * @param recordClass
+   *     class of {@link Record} where the result are referenced.
+   * @param attribute
+   *     attribute of {@code recordClass} used for the reference.
+   * @param <T>
+   *     type of {@link Named} to list.
+   * @param <R>
+   *     type of {@link Record} where the result are referenced.
+   * @return the requested list, ordered by decreasing reference
+   * number, then by ascending name.
+   */
+  private <T extends Named, R extends Record> List<T> list(
+      Class<T> resultClass,
+      Class<R> recordClass,
+      String attribute
+  ) {
+    CriteriaQuery<T> query = builder.createQuery(null);
+    Root<R> record = query.from(recordClass);
+    Root<T> result = query.from(resultClass);
+    Expression<Long> count = builder.count(result);
+    query.multiselect(result);
+    query.where(builder.equal(result, record.get(attribute)));
+    query.groupBy(result);
+    query.orderBy(builder.desc(count), builder.asc(result.get("name")));
+    return em.createQuery(query)
+        .getResultList();
   }
 
   /**
--- Model/src/test/java/fr/elimerl/registre/services/AutoCompleterTest.java
+++ Model/src/test/java/fr/elimerl/registre/services/AutoCompleterTest.java
@@ -1,24 +1,14 @@
 package fr.elimerl.registre.services;
 
 import fr.elimerl.registre.entities.Actor;
-import fr.elimerl.registre.entities.Author;
-import fr.elimerl.registre.entities.Composer;
-import fr.elimerl.registre.entities.Location;
+import fr.elimerl.registre.entities.Director;
 import fr.elimerl.registre.entities.Named;
-import fr.elimerl.registre.entities.Series;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,47 +16,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Test case for {@link AutoCompleter}.
  */
-@RunWith (Parameterized.class)
-@ContextConfiguration ("/applicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
 public class AutoCompleterTest {
-
-  @Parameters (name = "{0} in {1}")
-  public static Iterable<Object[]> parameters () {
-    return Arrays.asList (
-        new Object[] {"Wil", Actor.class, new String[] {"Will Smith"}},
-        new Object[] {"smi", Actor.class, new String[] {"Will Smith"}},
-        new Object[] {"S", Actor.class, new String[] {"Scarlett Johansson", "Will Smith"}},
-        new Object[] {"Tom", Author.class, new String[] {"Tom Clancy"}},
-        new Object[] {"h", Composer.class, new String[] {"Hans Zimmer", "Howard Shore"}},
-        new Object[] {"Mer", Series.class, new String[] {"Merlin"}},
-        new Object[] {"l", Location.class, new String[] {"La Roche sur Yon", "Lyon"}},
-        new Object[] {"%e%", Series.class, new String[] {}}
-    );
-  }
-
-  @ClassRule
-  public static final SpringClassRule springClassRule = new SpringClassRule ();
-
-  @Rule
-  public SpringMethodRule springMethodRule = new SpringMethodRule ();
-
-  @Parameter (0)
-  public String query;
-
-  @Parameter (1)
-  public Class<? extends Named> clazz;
-
-  @Parameter (2)
-  public String[] expected;
 
   @Resource
   private AutoCompleter autoCompleter;
 
+  /** Test {@link AutoCompleter#listActors()}. */
   @Test
-  public void shouldAutoComplete () {
-    List<String> actual = autoCompleter.autoComplete (query, clazz);
+  public void should_list_actors() {
+    List<Actor> actual = autoCompleter.listActors();
 
-    assertThat (actual).containsExactly (expected);
+    assertThat(actual)
+        .extracting(Named::getName)
+        .containsExactly(
+            "Scarlett Johansson",
+            "Emma Watson",
+            "Will Smith"
+        );
+  }
+
+  /** Test {@link AutoCompleter#listDirectors()}. */
+  @Test
+  public void should_list_directors() {
+    List<Director> actual = autoCompleter.listDirectors();
+
+    assertThat(actual)
+        .extracting(Named::getName)
+        .containsExactly("Luc Besson");
   }
 
 }
