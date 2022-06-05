85106dcef1836db25b7665edc5201b867580545a
Étienne Miret <etienne.miret@ens-lyon.org>
2022-06-05 22:18:58 +0200

    Define types for artworks.

---
+++ lib/mongo/artwork/types.ts
@@ -0,0 +1,45 @@
+import { ObjectID } from 'bson';
+
+export const enum ArtworkType {
+  Movie,
+  Comic,
+  Book,
+}
+
+interface BaseArtwork {
+  _id: ObjectID;
+  type: ArtworkType;
+  title: string;
+  series?: ObjectID;
+  number?: number;
+  comment?: string;
+  picture?: string;
+}
+
+export const enum Support {
+  K7,
+  DVD,
+  DEM,
+  BRD
+}
+
+export interface Movie extends BaseArtwork {
+  type: ArtworkType.Movie;
+  support: Support;
+  director: ObjectID[]; // Person
+  actors: ObjectID[]; // Person
+  composer: ObjectID[]; // Person
+}
+
+export interface Comic extends BaseArtwork {
+  type: ArtworkType.Comic;
+  cartoonist: ObjectID[]; // Person
+  scriptWriter: ObjectID[]; // Person
+}
+
+export interface Book extends BaseArtwork {
+  type: ArtworkType.Book;
+  author: ObjectID[]; // Person
+}
+
+export type Artwork = Movie | Comic | Book;
