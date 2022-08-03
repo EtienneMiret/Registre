8dc6ae2d6f5b6d3ba97ebb2f65558999819c3974
Étienne Miret <etienne.miret@ens-lyon.org>
2022-08-03 11:33:39 +0200

    Use Mongo typed collection.

--- lib/mongo/member/find.ts
+++ lib/mongo/member/find.ts
@@ -2,7 +2,6 @@ import db from '../db';
 import { Member } from './types';
 
 export async function findByEmail (email: string): Promise<Member | null> {
-  return db.then(db => db.collection('members'))
-      .then(members => members.findOne({emails: email}))
-      .then(doc => doc as (Member | null));
+  return db.then(db => db.collection<Member>('members'))
+      .then(members => members.findOne({emails: email}));
 }
