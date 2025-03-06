63eb994d3a1ab83301e06abbb2e75f0f484f0b49
Étienne Miret <etienne.miret@ens-lyon.org>
2025-03-06 22:08:31 +0100

    Mock UserRepository

--- back-end/services/mock_test.go
+++ back-end/services/mock_test.go
@@ -38,3 +38,39 @@ func (m *MockSessionRepository) FindById(
 	}
 	return sess.(*types.Session), args.Error(1)
 }
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) List(ctx context.Context) ([]*types.User, error) {
+	args := m.Called(ctx)
+	users := args.Get(0)
+	if users == nil {
+		return nil, args.Error(1)
+	}
+	return users.([]*types.User), args.Error(1)
+}
+
+func (m *MockUserRepository) Create(ctx context.Context, name string, emails ...string) (string, error) {
+	args := m.Called(ctx, name, emails)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockUserRepository) FindById(ctx context.Context, id string) (*types.User, error) {
+	args := m.Called(ctx, id)
+	user := args.Get(0)
+	if user == nil {
+		return nil, args.Error(1)
+	}
+	return user.(*types.User), args.Error(1)
+}
+
+func (m *MockUserRepository) FindByEmail(ctx context.Context, email string) (*types.User, error) {
+	args := m.Called(ctx, email)
+	user := args.Get(0)
+	if user == nil {
+		return nil, args.Error(1)
+	}
+	return user.(*types.User), args.Error(1)
+}
