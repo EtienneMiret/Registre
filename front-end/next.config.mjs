5f441e4e6e9bb110e8eebc2d94464d7e5e8f7131
Étienne Miret <etienne.miret@ens-lyon.org>
2025-03-08 16:35:09 +0100

    Proxy /api requests to backend

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: `${process.env.REGISTRE_BACKEND_URL}/:path*`,
+    }
+  ]
 };
 
 export default nextConfig;
