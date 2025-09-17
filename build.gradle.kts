782d7ba67dbb5c57e691d1518d369f5a9ed03784
Étienne Miret <etienne.miret@ens-lyon.org>
2025-09-17 08:06:46 +0200

    Add Kotlin Gradle plugin to root project
    Adding this plugin to several subprojects of a same build can break
    the build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+  val kotlinVersion = "2.2.20"
+  kotlin("jvm") version kotlinVersion apply false
+  kotlin("plugin.spring") version kotlinVersion apply false
+  kotlin("multiplatform") version kotlinVersion apply false
+}
+
 allprojects {
   group = "io.miret.registre"
   version = "4.0.0-SNAPSHOT"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
-pluginManagement {
-  plugins {
-    val kotlinVersion = "2.2.20"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
-    kotlin("multiplatform") version kotlinVersion
-  }
-}
-
 rootProject.name = "Registre"
 include("client")
 include("server")
