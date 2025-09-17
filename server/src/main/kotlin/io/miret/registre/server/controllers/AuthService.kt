0c554fe640bd3e204a8849409a82591ef5ddf4d2
Étienne Miret <etienne.miret@ens-lyon.org>
2025-09-17 04:29:18 +0200

    Add a backend with a simple auth endpoint

---
+++ server/src/main/kotlin/io/miret/registre/server/controllers/AuthService.kt
@@ -0,0 +1,14 @@
+package io.miret.registre.server.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
+class AuthService {
+
+  @GetMapping("/whoami")
+  fun whoami() = "anonymous"
+
+}
