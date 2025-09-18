04accb92ebd334eaab593c17dac5413a12707176
Étienne Miret <etienne.miret@ens-lyon.org>
2025-09-18 21:20:18 +0200

    Query the backend from the frontend

--- client/src/webMain/kotlin/io/miret/registre/client/App.kt
+++ client/src/webMain/kotlin/io/miret/registre/client/App.kt
@@ -6,8 +6,13 @@ import react.create
 import react.dom.client.createRoot
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.main
+import react.dom.html.ReactHTML.p
+import react.useEffectOnce
+import react.useState
 import web.dom.ElementId
 import web.dom.document
+import web.http.fetch
+import web.http.text
 
 fun main() {
   createRoot(document.getElementById(ElementId("root"))!!)
@@ -15,13 +20,37 @@ fun main() {
 }
 
 val App = FC<Props>("App") {
+  val (user, setUser) = useState("<NONE>")
+  val (status, setStatus) = useState(HttpStatus.INITIAL)
+
+  useEffectOnce {
+    setStatus(HttpStatus.LOADING)
+    val response = fetch("/api/auth/whoami")
+    if (response.ok) {
+      setUser(response.text())
+      setStatus(HttpStatus.SUCCESS)
+    } else {
+      setStatus(HttpStatus.ERROR)
+    }
+  }
+
   h1 {
     +"Registre"
   }
 
   main {
-    Welcome {
-      name = "World"
+    when (status) {
+      HttpStatus.INITIAL, HttpStatus.LOADING -> p {
+        +"Chargement en cours…"
+      }
+
+      HttpStatus.SUCCESS -> Welcome {
+        name = user
+      }
+
+      HttpStatus.ERROR -> p {
+        +"Erreur lors de la connexion ☹️."
+      }
     }
   }
 }
---
+++ client/src/webMain/kotlin/io/miret/registre/client/HttpStatus.kt
@@ -0,0 +1,8 @@
+package io.miret.registre.client
+
+enum class HttpStatus {
+  INITIAL,
+  LOADING,
+  SUCCESS,
+  ERROR,
+}
