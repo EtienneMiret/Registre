1576c67edac6600b001a899699842567328055bb
Étienne Miret <etienne.miret@ens-lyon.org>
2025-12-07 13:33:41 +0100

    Fix production build

--- frontend/webpack.config.d/dev-server.js
+++ frontend/webpack.config.d/dev-server.js
@@ -1,8 +1,10 @@
-config.devServer.port = 3000;
-config.devServer.proxy = [
-  {
-    context: ['/api'],
-    target: 'http://localhost:8080',
-    pathRewrite: { '^/api': '' },
-  },
-];
+if (config.devServer) {
+  config.devServer.port = 3000;
+  config.devServer.proxy = [
+    {
+      context: ['/api'],
+      target: 'http://localhost:8080',
+      pathRewrite: {'^/api': ''},
+    },
+  ];
+}
