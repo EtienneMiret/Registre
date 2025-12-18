f52fce8a24d39e9eb9018f3daf08fcbb06ecca6c
Étienne Miret <etienne.miret@ens-lyon.org>
2025-12-18 18:22:49 +0100

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
   /* Common */
-  const val kotlin = "2.2.20"
+  const val kotlin = "2.3.0"
   const val kotlinxSerialization = "1.9.0"
 
   /* Frontend */
   const val reactRedux = "9.2.0"
-  const val reduxToolkit = "2.11.1"
-  const val kotlinWrappers = "2025.9.8"
+  const val reduxToolkit = "2.11.2"
+  const val kotlinWrappers = "2025.12.7"
 
   /* Backend */
-  const val springBoot = "3.5.5"
+  const val springBoot = "4.0.0"
   const val springDependencyManagement = "1.1.7"
 }
