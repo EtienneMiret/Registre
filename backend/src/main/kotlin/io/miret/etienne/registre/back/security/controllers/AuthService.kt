799a22a5807b8f2c7cca9cdcd93fc0c57cc3a560
Étienne Miret <etienne.miret@ens-lyon.org>
2026-01-03 14:55:05 +0100

    Display name of logged in user

--- backend/src/main/kotlin/io/miret/etienne/registre/back/security/controllers/AuthService.kt
+++ backend/src/main/kotlin/io/miret/etienne/registre/back/security/controllers/AuthService.kt
@@ -1,15 +1,19 @@
 package io.miret.etienne.registre.back.security.controllers
 
-import io.miret.etienne.registre.common.User
+import io.miret.etienne.registre.back.security.model.User
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import io.miret.etienne.registre.common.User as ApiUser
 
 @RestController
 @RequestMapping("/auth")
 class AuthService {
 
   @GetMapping("/whoami")
-  fun whoami(): User = User("anonymous", "Anonymous")
+  fun whoami(
+    @AuthenticationPrincipal user: User,
+  ): ApiUser = ApiUser(user.id, user.name)
 
 }
